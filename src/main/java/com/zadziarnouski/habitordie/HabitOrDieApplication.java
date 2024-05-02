8a12986954645e27c565eb0f86c11ff4e85479a8
Taras Zadziarnouski <taras.zadziarnouski@gmail.com>
2024-05-02 15:27:22 +0200

    Init project

---
+++ src/main/java/com/zadziarnouski/habitordie/HabitOrDieApplication.java
@@ -0,0 +1,13 @@
+package com.zadziarnouski.habitordie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitOrDieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitOrDieApplication.class, args);
+    }
+
+}
